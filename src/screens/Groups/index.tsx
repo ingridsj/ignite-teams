5ff4fec39bc1b26fcc5eb74c720fffdcfed0806c
ingridsj <ingridsjalmeida@gmail.com>
2023-05-29 20:13:44 -0300

    criando componente list empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,9 +5,10 @@ import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { useState } from "react";
 import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <S.Container>
@@ -17,6 +18,9 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </S.Container>
   );
