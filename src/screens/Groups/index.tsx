86503744a27ec88715c0f862ced66fffd4bccfb0
ingridsj <ingridsjalmeida@gmail.com>
2023-06-06 18:19:57 -0300

    add função de navigate

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,22 @@
-import { Header } from "@components/Header";
-
-import * as S from "./styles";
-import { Highlight } from "@components/Highlight";
-import { GroupCard } from "@components/GroupCard";
 import { useState } from "react";
 import { FlatList } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
 
+import * as S from "./styles";
+
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
+
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate("newGroup");
+  }
 
   return (
     <S.Container>
@@ -24,7 +31,7 @@ export function Groups() {
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
-      <Button title="Criar nova turma" />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </S.Container>
   );
 }
